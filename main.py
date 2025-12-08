a723b7816f2e532f139a44b01cd5dbb8f4bf0778
sj483-del <sjohnston483@gmail.com>
2025-12-08 21:11:00 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from src.dungeon import Dungeon
+
+def main():
+    print("=== Welcome to the Game Prototype ===")
+    dungeon = Dungeon()
+    dungeon.start()
+
+if __name__ == "__main__":
+    main()
 # main game entry point
